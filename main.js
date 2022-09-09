ebb3e720ff4482250f3225d7010fc3ef199e1797
Ahmed-A-Hessuin <89793995+Ahmed-A-Hessuin@users.noreply.github.com>
2022-09-09 23:27:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// let myImage = document.querySelectorAll("div img");
+// for (let i = 0; i < myImage.length ; i++) {
+//     myImage[i].setAttribute("src", "https://elzero.org/wp-content/themes/elzero/imgs/logo.png");
+//     myImage[i].setAttribute("alt", "Elzero Logo");
+//     console.log(myImage[i]);
+// }
+
+// let myInput = document.querySelector("[name='dollar']");
+// let result = document.querySelector('.result');
+// myInput.oninput = function () {
+//     result.textContent =
+//         `{${myInput.value}} USD Dollar =
+//     {${(myInput.value * 15.6).toFixed(2)}} Egyptian Pound`
+// }
+
+// let ele1 = document.querySelector(".one");
+// let ele2 = document.querySelector(".two");
+
+// let textele1 = document.createTextNode("One");
+// ele1.appendChild(textele1);
+
+// ele1.setAttribute("title", "one");
+
+
+// console.log(ele1);
+// console.log(ele2);
